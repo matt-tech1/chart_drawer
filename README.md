e97bcf9c48cd997fd2ae8e1f4de3ab78714f1061
Szymon Siemieniuk <szymonsiemieniuk01@gmail.com>
2020-06-10 20:53:52 +0200

    TEST

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chart drawer v0.1 BETA
+Program do rysowania wykresów funkcji. W trakcie budowy.
---
+++ src/chart/main.cpp
@@ -0,0 +1,20 @@
+#include "wx/wx.h"
+class HelloWorldApp : public wxApp
+{
+public:
+	virtual bool OnInit()
+	{
+		wxFrame* frame = new wxFrame((wxFrame*)NULL, -1, _T("Hello wxWidgets World"));
+		frame->CreateStatusBar();
+		frame->SetStatusText(_T("Hello World"));
+		frame->Show(true);
+		SetTopWindow(frame);
+		return true;
+	}
+};
+
+DECLARE_APP(HelloWorldApp)
+
+int main() {
+	return 0;
+}
